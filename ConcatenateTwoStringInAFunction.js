609159ef9b1488d27bd6c569a0f644f0783a8b17
39sanskar <mishrasanskar496@gmail.com>
2024-05-17 16:50:20 +0530

    Add two string using function

---
+++ ConcatenateTwoStringInAFunction.js
@@ -0,0 +1,9 @@
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greetLinda(){
+    console.log(greeting + "," + name + "!" )
+}
+greetLinda()
