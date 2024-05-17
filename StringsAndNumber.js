b3991e5aa49c52381fc3adb6d93461671b061f23
39sanskar <mishrasanskar496@gmail.com>
2024-05-17 17:03:37 +0530

    Learn some important concept in javascript

---
+++ StringsAndNumber.js
@@ -0,0 +1,10 @@
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // irt return string "22"
+console.log(11 + 7) // 18
+console.log(6 + "5") // it teturn string "65"
+console.log("My points: " + 5 + 9) //  it will be return "My points: 59"
+// here the above example everything is String ...
+console.log(2 + 2) //  it will be return 4
+console.log("11" + "14") // "1114"
+
+// when a fight between string and number alwaya string will we win.
