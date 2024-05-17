269a7f4f95a2e6f85ed71cdb1483e26b37793d25
39sanskar <mishrasanskar496@gmail.com>
2024-05-17 16:44:34 +0530

    first commit

---
+++ Variables-Practise.js
@@ -0,0 +1,8 @@
+// Create two variables, firstName and lastName
+let firstName = "Sanskar"
+let lastName = "Mishra"
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName 
+// Log fullName to the console
+console.log(fullName)
+
